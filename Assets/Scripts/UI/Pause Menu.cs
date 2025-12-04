62ee635af97756082bd878fed1f18259ae48c7c2
gsalvatierra-dv <angel.salvatierra@davinci.edu.ar>
2025-12-04 01:32:00 -0300

    Pause Menu

--- Assets/Scripts/UI/Pause Menu.cs
+++ Assets/Scripts/UI/Pause Menu.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Panels")]
+    [SerializeField] private GameObject _pauseButton;
+    [SerializeField] private GameObject _pauseMenuPanel;
+    [SerializeField] private GameObject _soundPanel;
+    [SerializeField] private GameObject _currencyIndicator;
+    [SerializeField] private GameObject _staminaIndicator;
+
+    public void Pause()
     {
-        
+        SFXManager.Instance.PlaySFX(SFXManager.SFXCategoryType.ButtonPress);
+        _pauseMenuPanel.SetActive(true);
+        _currencyIndicator.SetActive(true);
+        _staminaIndicator.SetActive(true);
+
+        _pauseButton.SetActive(false);
+        _soundPanel.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Resume()
     {
-        
+        SFXManager.Instance.PlaySFX(SFXManager.SFXCategoryType.ButtonPress);
+        _pauseButton.SetActive(true);
+
+        _pauseMenuPanel.SetActive(false);
+        _soundPanel.SetActive(false);
+        _currencyIndicator.SetActive(false);
+        _staminaIndicator.SetActive(false);
     }
+
+    public void ShowSoundPanel()
+    {
+        SFXManager.Instance.PlaySFX(SFXManager.SFXCategoryType.ButtonPress);
+        _soundPanel.SetActive(true);
+
+        _pauseMenuPanel.SetActive(false);
+        _currencyIndicator.SetActive(false);
+        _staminaIndicator.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+    Application.Quit();
+#endif
+    }
+
 }
